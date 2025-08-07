b39208f1fa63d6abd8a6c19fd06e393ed8fdea18
PoornimaChowdary560 <poornimamaddipati25@gmail.com>
2025-08-07 15:51:13 +0530

    17545

--- home/views.py
+++ home/views.py
@@ -1,16 +1,14 @@
-class Order(models.Model):
-    STATUS_CHOICES = [
-        ('PENDING','pending'),
-        ('PREPARING', 'preparing'),
-        ('DELIVERED', 'delivered'),
-        ('CANCELLED', 'cancelled'),
-    ]
-    customer=models.ForeingKey(User,on_delete=models.CASCADE, related_name='orders')
-    items=models.ManyToManyField(MenuItem,related_name='orders')
-    total_amount=models.DecimalField(max_digits=8,decimal_places=2)
-    status=models.CharField(max_length=20, CHOICES=STATUS_CHOICES,default='PENDING')
-    def __str__(self):
-        return f"Order #{self.id} by {self.customer.username}"
-    
-    def calculate_total(self):
-        return sum(item.price for item in self.items.all())
+from django.contrib import admin
+from .models import MenuItem, Order
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display=('name','price','is_available')
+    search_fields=('name',)
+    list_filter=('is_available',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=('id','customer','status', 'total_amount')
+    list_filter=('status',)
+    search_fields=('customer_username',)
+    filter_horizantal=('items',)
