91fea74d32757fd2078067c180a96471c6fa8cea
PoornimaChowdary560 <poornimamaddipati25@gmail.com>
2025-08-07 15:44:00 +0530

    17544

--- home/views.py
+++ home/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
-import requests
-def homepage(request):
-    try:
-        response=request.get('http://localhost:8000/api/menu')
-        menu_items=response.json()
-    except requests.exceptions.RequestException:
-        menu_items=[]
-    return render(request,'menu/homepage.html', {'menu_items':menu_items})
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING','pending'),
+        ('PREPARING', 'preparing'),
+        ('DELIVERED', 'delivered'),
+        ('CANCELLED', 'cancelled'),
+    ]
+    customer=models.ForeingKey(User,on_delete=models.CASCADE, related_name='orders')
+    items=models.ManyToManyField(MenuItem,related_name='orders')
+    total_amount=models.DecimalField(max_digits=8,decimal_places=2)
+    status=models.CharField(max_length=20, CHOICES=STATUS_CHOICES,default='PENDING')
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.username}"
+    
+    def calculate_total(self):
+        return sum(item.price for item in self.items.all())
