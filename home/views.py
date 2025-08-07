f51189d1af9549de36f1109ec2a405d378a7a976
PoornimaChowdary560 <poornimamaddipati25@gmail.com>
2025-08-07 15:11:45 +0530

    17543

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+import requests
+def homepage(request):
+    try:
+        response=request.get('http://localhost:8000/api/menu')
+        menu_items=response.json()
+    except requests.exceptions.RequestException:
+        menu_items=[]
+    return render(request,'menu/homepage.html', {'menu_items':menu_items})
