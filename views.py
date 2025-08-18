61518bf0668acc93b556576509e9ca8f23d93515
PoornimaChowdary560 <poornimamaddipati25@gmail.com>
2025-08-18 14:10:19 +0530

    33296

--- views.py
+++ views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import MenuItem
 def menu_list(request):
     menu_items=[
         {"name":"pizza", "price":8.99, "description":"classic cheese pizza with tomato sauce"},
@@ -6,3 +8,10 @@ def menu_list(request):
         {"name":"pasta Alfredo", "price":7.99, "description":"creamy alfredo sauce over fettuccine pasta"}
     ]
     return render(request, 'menu_list.html', {'menu_items':menu_items})
+
+def menu_view(request):
+    try:
+        items=MenuItem.objects.all()
+        return render(request, 'menu.html', {'items':items})
+    except Exception as e:
+        return HttpResponse(f"As error occured while loading the menu {str(e)}", status=500)
