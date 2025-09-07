54d887d4486ae6528a47e300539f238c5a24559b
PoornimaChowdary560 <poornimamaddipati25@gmail.com>
2025-09-07 13:32:30 +0530

    42487

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -3,7 +3,7 @@ class MenuItem(models.Model):
     name=models.CharField(max_length=100)
     description=models.TextField(blank=True)
     price=models.DecimalField(max_digits=6, decimal_places=2)
-    image=models.ImageField(upload_to='menu_images/' , blank=True, Null=True)
+    image=models.ImageField(upload_to='menu_image/' , blank=True, Null=True)
 
     def __str__(self):
         return self.name
